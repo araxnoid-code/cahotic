6f7913cc038e6c1ecd18245296473c8810c3990f
araxnoid <araxnoid0@gmail.com>
2026-04-10 00:07:35 +0700

    added testing code for schedule

--- src/cahotic/testing/schedule.rs
+++ src/cahotic/testing/schedule.rs
@@ -165,22 +165,45 @@ fn schedule_5() {
 
     let mut poll_1 = cahotic.scheduling_create_initial(MyTask::Task(|| {
         sleep(Duration::from_millis(500));
+        println!("done 1");
         MyOutput::Result(10)
     }));
+
     let mut poll_2 = cahotic.scheduling_create_schedule(MyTask::Schedule(|schedule_vec| {
         if let Some(MyOutput::Result(x)) = schedule_vec.get(0) {
             assert_eq!(*x, 10);
         } else {
             panic!("Schedule Testing Error")
         }
+        println!("done 2");
 
-        MyOutput::None
+        MyOutput::Result(20)
     }));
 
     cahotic.schedule_after(&mut poll_2, &mut poll_1).unwrap();
 
-    cahotic.schedule_exec(poll_2);
+    let mut poll_3 = cahotic.scheduling_create_schedule(MyTask::Schedule(|schedule_vec| {
+        if let Some(MyOutput::Result(x)) = schedule_vec.get(0) {
+            assert_eq!(*x, 20);
+        } else {
+            panic!("Schedule Testing Error")
+        }
+        println!("done 3");
+
+        MyOutput::None
+    }));
+
+    cahotic.schedule_after(&mut poll_3, &mut poll_2).unwrap();
+
+    sleep(Duration::from_millis(1000));
+
     cahotic.schedule_exec(poll_1);
+    sleep(Duration::from_millis(1000));
+
+    sleep(Duration::from_millis(1000));
+    cahotic.schedule_exec(poll_2);
+    sleep(Duration::from_millis(1000));
+    cahotic.schedule_exec(poll_3);
 
     cahotic.join();
 }
